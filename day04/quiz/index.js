444c4c47c3b4ec0f4c01ba8258393e97fe2eda59
joshua <iuttn123@gmail.com>
2025-02-04 15:17:03 +0900

    finish

--- day04/quiz/index.js
+++ day04/quiz/index.js
@@ -1,18 +1,4 @@
-import { changeLight } from "./func.js";
+import { elements } from "./elements.js";
+import { handlerInputEvent } from "./handler.js";
 
-const input = document.querySelector("#input");
-const special = document.querySelector("#special");
-const charLength = document.querySelector("#charLength");
-const checkButton = document.querySelector("#checkButton");
-
-const specialList = [..."!@#$"];
-
-input.addEventListener("input", (e) => {
-  const { value } = e.target;
-  const { length } = value;
-  const isLengthValid = 6 <= length && length <= 20;
-  const isSpecialValid = specialList.some((v) => value.includes(v));
-  changeLight(charLength, isLengthValid);
-  changeLight(special, isSpecialValid);
-  changeLight(checkButton, isLengthValid && isSpecialValid);
-});
+elements.input.addEventListener("input", (e) => handlerInputEvent(e));
